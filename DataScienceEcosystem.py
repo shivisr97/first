619deb294ebc3858901e26600927c62dc95077f9
Shivam Pandey <143844185+shivisr97@users.noreply.github.com>
2023-09-02 02:25:35 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+
+# In[1]:
+
+
+intro_sentence = "In this notebook, Data Science Tools and Ecosystem are summarized."
+print(intro_sentence)
+
+
+# In[2]:
+
+
+
+popular_languages = ["Python", "R", "SQL", "Julia"]
+for language in popular_languages:
+    print(language)
+
+
+# In[ ]:
+
+
+
+
